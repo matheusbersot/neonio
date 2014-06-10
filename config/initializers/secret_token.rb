a773ea456acd9a852c562f730c10bf500c6c0be3
Matheus Bersot <matheusbersotsb@gmail.com>
2014-06-09 21:56:56 -0300

    closes #1 por matheusbersot: criação do projeto inicial rails configurado para banco de dados.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neonio::Application.config.secret_token = '6e668b608330a863467489a0e3eeec9187b03b7bf0a0980c8b826ae411202126da03af476c19e2f1cc14e8b022179005b649900c07640e9ddd0622819c0b6fc7'
